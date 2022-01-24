b3bbd9507a849107c2b764941154c64c4fd7d864
Olga Konieczko <konieczko.olga@gmail.com>
2022-01-24 11:32:52 +0100

    Dodanie pierwszego SPOJa

---
+++ DodawanieLiczbCałkowitych/DodawanieLiczbCałkowitych/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DodawanieLiczbCałkowitych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            int wynik;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            wynik = a + b + c;
+            Console.WriteLine(wynik);
+
+        }
+    }
+}
