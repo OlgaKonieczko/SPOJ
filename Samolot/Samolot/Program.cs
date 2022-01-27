b8cef23ebd12f53b437d778bbf127bdf496663b2
Olga Konieczko <konieczko.olga@gmail.com>
2022-01-27 18:47:39 +0100

    Kolejne zadanie

---
+++ Samolot/Samolot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Samolot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string miejsca = Console.ReadLine();
+            string[] numbers = miejsca.Split(" ");
+            int n1 = Convert.ToInt32(numbers[0]);
+            int n2 = Convert.ToInt32(numbers[2]);
+            int k1 = Convert.ToInt32(numbers[1]);
+            int k2 = Convert.ToInt32(numbers[3]);
+            int suma = n1 * k1 + n2 * k2;
+            Console.WriteLine(suma.ToString());
+            
+        }
+    }
+}
